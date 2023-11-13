88bed8d78025dd2ae1a2e44d552a417acaf8c1f8
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-13 10:35:52 +0100

    Add initial product information to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let nomProduit = 'T-shirt simple';
+var quantiteStock = 10;
+const prixProduit = 5;
+let enPromotion = false;
+
+console.log(nomProduit + " " + quantiteStock + " " + prixProduit + " " + enPromotion);
