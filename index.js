6fb4fac9d0c1f99be3b9f0b0a9365b84e453cf16
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-13 10:48:30 +0100

    Add HTML and JavaScript code for displaying product information.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 
-let nomProduit = 'T-shirt simple';
-var quantiteStock = 10;
-const prixProduit = 5;
-let enPromotion = false;
 
-console.log(nomProduit + " " + quantiteStock + " " + prixProduit + " " + enPromotion);
+addEventListener("click", function() {
+    afficherProduit();
+});
+
+
+
+function afficherProduit() {
+
+    let nomProduit = 'T-shirt simple';
+    var quantiteStock = 10;
+    const prixProduit = 5;
+    let enPromotion = false;
+    
+    console.log("nomProduit: " + nomProduit);
+    console.log("quantiteStock: " + quantiteStock);
+    console.log("prixProduit: " + prixProduit);
+    console.log("enPromotion: " + enPromotion);
+}
