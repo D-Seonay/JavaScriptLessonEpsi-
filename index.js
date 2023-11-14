8b94780fff16c71e1348c5bec4aed71ecc9ce00d
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-14 09:54:51 +0100

    Add Exercise 3 button and function to calculate purchase reduction based on category

--- index.js
+++ index.js
@@ -43,6 +43,47 @@ console.log("prixMoyen " + prixMoyen.toFixed ("€") + "\nobejctVentesAtteint: "
 }
 
 
+const buttonExercise3 = document.getElementById("exercise3");
+
+
+buttonExercise3.addEventListener("click", function() {
+    exercise3();
+});
+function exercise3 () {
+let montantTotalAchat = 180;
+let reduction = 0;
+
+    switch (choixCategorie) {
+        case "éléctronique":
+            console.log("Vous avez choisi la catégorie éléctronique");
+            break;
+        case "vêtement":
+            console.log("Vous avez choisi la catégorie vêtement");
+            break;
+        case "alimentation":
+            console.log("Vous avez choisi la catégorie alimentation");
+            break;
+        default:
+            console.log("Désolé, cette catégorie n'existe pas.");
+            break;
+    }
+    
+    if (choixCategorie == "éléctronique") {
+        let reduction = montantTotalAchat * 0.1;
+        console.log("Vous avez choisi la catégorie éléctronique");
+    }
+    else if (choixCategorie == "vêtement") {
+        let reduction = montantTotalAchat * 0.2;
+        console.log("Vous avez choisi la catégorie vêtement");
+    }
+    else if (choixCategorie == "alimentation") {
+        let reduction = montantTotalAchat * 0.3;
+        console.log("Vous avez choisi la catégorie alimentation");
+    }
+    else {
+        console.log("Désolé, cette catégorie n'existe pas.");
+    }
+}
 
 
 
